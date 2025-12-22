ebd39ca42520be08f871f51b8aa70ad6e512cc89
Canh <thuhienhc2020@gmail.com>
2025-12-22 23:43:19 +0700

    fix

--- src/components/ATpage/IntroSection.jsx
+++ src/components/ATpage/IntroSection.jsx
@@ -1,7 +1,7 @@
 const IntroSection = () => {
   return (
     <div className="bg-[url('ATpage/bgIntroAT.jpg')] bg-cover bg-center bg-no-repeat h-[100vh] flex items-center justify-center">
-      <p className="w-[60%] text-5xl text-gray-800 font-['Itim'] leading-relaxed">
+      <p className="w-[60%] lg:text-5xl md:text-4xl text-3xl text-gray-800 font-['Itim'] leading-relaxed">
         Ít ai nhìn thấy những khoảng lặng phía sau những tấm giấy khen và lời ghi
         nhận. Có một cô gái đã đi qua giảng đường với nhịp bước bền bỉ, gánh
         trên vai nhiều trách nhiệm hơn tuổi của mình. Trong hành trình ấy, có
